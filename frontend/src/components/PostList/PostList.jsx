b45891febb83da4b3de2a612148e4f437dbedcc8
Alexis Thierry <alexist103@gmail.com>
2024-03-21 23:35:42 +0100

    Adding routes

--- frontend/src/components/PostList/PostList.jsx
+++ frontend/src/components/PostList/PostList.jsx
@@ -1,60 +1,19 @@
 import Post from "../Post/Post.jsx";
 import classes from "./PostList.module.css"
-import NewPost from "../NewPost/NewPost.jsx";
-import Modal from "../Modal/Modal.jsx";
 import {useState, useEffect} from "react";
+import {useLoaderData} from "react-router-dom";
 
-function PostList({isPosting, onStopPosting}) {
-
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-
-    useEffect(() => {
-        async function fetchPosts() {
-            setIsFetching(true);
-            const res = await fetch('http://localhost:8080/posts/');
-            const resData = await res.json();
-            setPosts(resData.posts);
-            setIsFetching(false);
-        }
-
-        fetchPosts().then(r => {
-            console.log("posts loaded");
-        });
-    }, []);
-
-    function addPostHandler(postData) {
-        fetch('http://localhost:8080/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type': "application/json"
-            }
-        })
-            .then(res => console.log("post sended"))
-            .catch(error => console.log({error}));
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
+function PostList() {
+    const posts = useLoaderData();
 
     return (
         <>
-            {isPosting && (
-                <Modal isVisible={isPosting} onClose={onStopPosting}>
-                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
-                </Modal>
-            )}
-            {isFetching && (
-                <div style={{textAlign: "center", color: "white"}}>
-                    <h2>Chargement des posts.</h2>
-                    <p>Veuillez patienter ...</p>
-                </div>
-            )}
-            {!isFetching && posts.length > 0 && (
+            {posts.length > 0 && (
                 <ul className={classes.post}>
                     {posts.map((post, index) => <Post key={index} body={post.body} author={post.author}></Post>)}
                 </ul>
             )}
-            {!isFetching && posts.length === 0 && (
+            {posts.length === 0 && (
                 <div style={{textAlign: "center", color: "white"}}>
                     <h2>Pas encore de posts.</h2>
                     <p>Commence à poster dès maintenant !</p>
